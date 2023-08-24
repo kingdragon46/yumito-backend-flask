a39087e992a7af569d6bbe3f7a81fdd03e2c6d04
Vivek Anand <67418675+kingdragon46@users.noreply.github.com>
2023-08-24 13:24:46 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,34 +3,36 @@
 # Set execute permission for this script
 chmod +x "$0"
 
-# Update Python (assuming you're using apt-get)
-# sudo apt-get update  # This line requires root access
-# sudo apt-get install --only-upgrade python3  # This line requires root access
-
-# exit on error
+# Exit on error
 set -o errexit
 
 # Get the current directory (your Flask project directory)
 PROJECT_DIR="$(pwd)"
 
+# Check if 'requirements.txt' is present in the current directory
+requirements_txt="$PROJECT_DIR/requirements.txt"
+
+if [ ! -f "$requirements_txt" ]; then
+    echo "Error: 'requirements.txt' not found in the current directory."
+    exit 1
+fi
+
 # Create a virtual environment (if it doesn't already exist)
 if [ ! -d "$PROJECT_DIR/venv" ]; then
-   python -m venv "$PROJECT_DIR/venv"
+    python -m venv "$PROJECT_DIR/venv"
 fi
 
 # Activate the virtual environment
 source "$PROJECT_DIR/venv/bin/activate"
 
-echo "Env activated"
-
-# Navigate to your Flask project directory
-cd $PROJECT_DIR
+echo "Virtual environment activated."
 
 # Upgrade pip
-python -m pip install --upgrade pip
+pip install --upgrade pip
 
 # Install project dependencies from requirements.txt
-pip install -r requirements.txt
+pip install -r "$requirements_txt"
 
 # Run the Flask application using Gunicorn
+# Adjust the following line to match your Flask app's entry point
 # gunicorn -w 4 -b 0.0.0.0:8000 app2:app2
