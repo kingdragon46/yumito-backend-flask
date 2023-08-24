00098edeb0b846ae78233fbf8410597842caea5a
Vivek Anand <67418675+kingdragon46@users.noreply.github.com>
2023-08-24 12:50:00 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@ source "$PROJECT_DIR/venv/bin/activate"
 python -m pip install --upgrade pip
 
 # Install project dependencies from requirements.txt
-pip install -r "$PROJECT_DIR/requirements1.txt"
+pip install -r ./requirements1.txt
 
 # Run the Flask application using Gunicorn
 # gunicorn -w 4 -b 0.0.0.0:8000 app2:app2
