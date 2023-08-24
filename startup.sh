2b1aa53062b956fb95e92e2f6f99539213d003a1
Vivek Anand <67418675+kingdragon46@users.noreply.github.com>
2023-08-24 13:33:42 +0530

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Set execute permission for this script
+chmod +x "$0"
+
+# Exit on error
+set -o errexit
+
+# Get the current directory (your Flask project directory)
+PROJECT_DIR="$(pwd)"
+
+# Navigate to your Flask project directory
+cd "$PROJECT_DIR" || exit 1
+
+# Activate the virtual environment
+source "$PROJECT_DIR/venv/bin/activate"
+
+# Run the Flask application using Gunicorn
+# Adjust the following line to match your Flask app's entry point
+gunicorn -w 4 -b 0.0.0.0:8000 app2:app2
